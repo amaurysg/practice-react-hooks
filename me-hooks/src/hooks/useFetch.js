798a5cb2c0a8a9367c7712d1a399f88fe8e10549
Amaury Soto <amaurysotog@gmail.com>
2021-03-22 14:57:49 -0500

    useFetch - test

--- me-hooks/src/hooks/useFetch.js
+++ me-hooks/src/hooks/useFetch.js
@@ -1,52 +1,51 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 const useFetch = (url) => {
-
   //create useRef
-  const isMounted = useRef(true)
+  const isMounted = useRef(true);
 
-  const [state, setState] = useState({data:null, loading: true, error:null})
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
 
   //This useEffect only for unmount component
   useEffect(() => {
-    //this return only act when component is unmount 
-    return ()=>{
-      isMounted.current = false
-    }
-  }, [])
-
+    //this return only act when component is unmount
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
-    //Reset values 
+    //Reset values
     // setState({data:null, loading:true, error:null})
 
     //Fetch
     fetch(url)
-      .then(resp => resp.json())
-      .then(data =>{
-
-        
-            //This condicion stated ref of component 
-            if(isMounted.current){
-              setState({     
-              loading: false,
-              error: null,
-              data
-              
-            })
-
-            }else {
-              console.log('set stated not call')
-            }
-
-
-
+      .then((resp) => resp.json())
+      .then((data) => {
+        //This condicion stated ref of component
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        }
       })
-//In action only when url change
-  }, [url])
+      .catch(() => {
+        setState({
+          loading: false,
+          error: "No se encontró la info 😧",
+          data: null,
+        });
+      });
+    //In action only when url change
+  }, [url]);
 
+  return state;
+};
 
-  return state
-}
-
-export default useFetch
+export default useFetch;
--- me-hooks/src/tests/hooks/useForm.test.js
+++ me-hooks/src/tests/hooks/useForm.test.js
@@ -2,6 +2,7 @@ import { act, renderHook } from "@testing-library/react-hooks";
 import useForm from "../../hooks/useForm";
 
 describe("", () => {
+  //First, inital object with values, same as initialForm from component
   const initialForm = {
     name: "amaury",
     email: "amaurysotog@gmail.com",
@@ -25,19 +26,26 @@ describe("", () => {
     //in this case called the useForm with initialForm for above.
     const { result } = renderHook(() => useForm(initialForm));
     //Desestructure of array of values from {result.current}
+    // (,) this is values... too i can called as values.
     const [, handleInputChange] = result.current;
 
+    //Here simulated action in function handleInputChange
     act(() => {
       handleInputChange({
+        //Called target, target is paramet in main function
         target: {
           name: "name",
+          //Modifiqued value by Fernando
           value: "Fernando",
         },
       });
     });
 
+    //Here called values  {name: 'Fernando', email: 'amaurysotog@gmail.com}
     const [values] = result.current;
-    // console.log(values);
+    // console.log(values) output // {name: 'Fernando', email: 'amaurysotog@gmail.com};
+
+    //expect values with ...values but name in 'Fernando', other values equals
     expect(values).toEqual({ ...initialForm, name: "Fernando" });
 
     // expect(values).toEqual({ ...initialForm, name: "Fernando" });
@@ -47,8 +55,8 @@ describe("", () => {
     //here used renderHooks from react-hooks
     //in this case called the useForm with initialForm for above.
     const { result } = renderHook(() => useForm(initialForm));
-    //Desestructure of array of values from {result.current}
-    const [, handleInputChange, reset] = result.current;
+    //Desestructure of array of values from {result.current}  (,) values, handleInputChange reser
+    const [values, handleInputChange, reset] = result.current;
 
     act(() => {
       handleInputChange({
@@ -57,10 +65,11 @@ describe("", () => {
           value: "Fernando",
         },
       });
+      //here simulated reset
       reset();
     });
 
-    const [values] = result.current;
+    // const [values] = result.current;
     // console.log(values);
     expect(values).toEqual(initialForm);
   });
