e1a781ffc6584a9f398ef955e7f9eabdd129d130
Amaury Soto <amaurysotog@gmail.com>
2021-03-24 21:25:42 -0500

    comments in test

--- me-hooks/src/tests/components/useReducer/TodoList.test.js
+++ me-hooks/src/tests/components/useReducer/TodoList.test.js
@@ -3,6 +3,7 @@ import TodoList from "../../../components/useReducer/TodoList";
 import demoTodos from "../../fixtures/demoTodos";
 
 describe("Test in component <TodoList />", () => {
+  //Defined the functions.
   const handleDelete = jest.fn();
   const handleToogle = jest.fn();
   const handleUpdate = jest.fn();
@@ -21,6 +22,8 @@ describe("Test in component <TodoList />", () => {
   });
   test(" Debe tener dos <TodoListItem/> ", () => {
     expect(wrapper.find("TodoListItem").length).toBe(demoTodos.length);
+    const hola = wrapper.find("TodoListItem").at(0).html();
+    console.log(hola);
     expect(wrapper.find("TodoListItem").at(0).prop("handleDelete")).toEqual(
       expect.any(Function)
     );
